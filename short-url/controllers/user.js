401c13554201d685c93ee0b55fd34c54bb27a7dc
Srivastav-Anmol <anmolsrivastavami4@gmail.com>
2025-03-13 14:56:51 +0530

    fifth

--- short-url/controllers/user.js
+++ short-url/controllers/user.js
@@ -18,9 +18,8 @@ async function handleUserLogin(req,res){
         return res.render('login',{
     error:"Invalid username or password"
     })
-    const sessionId=uuidv4()
-    setUser(sessionId,user)
-    res.cookie('uid',sessionId)
+    const token = setUser(user)
+    res.cookie('uid',token)
     return res.redirect("/")//idhr se redirect ho jaoage url shortner website pe
 }
 module.exports={
--- short-url/service/auth.js
+++ short-url/service/auth.js
@@ -1,10 +1,19 @@
-const sessionIdToUserMap=new Map()
+const jwt=require("jsonwebtoken")
+const secretKey="12345@abcdefgh%^&*("
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user)
+function setUser(user){
+    return jwt.sign({
+        _id:user._id,
+        email:user.email,
+    },secret)
 }
-function getUser(id){
-    return sessionIdToUserMap.get(id)
+function getUser(token){
+    if(!token) return null
+    try{
+        return jwt.verify(token,secret)
+    }catch(error){
+        return null
+    }
 }
 module.exports={
     setUser,
