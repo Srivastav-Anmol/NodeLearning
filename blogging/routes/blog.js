9651964c236d999dcdc4dd8759e4597f25a93292
Srivastav-Anmol <anmolsrivastavami4@gmail.com>
2025-04-16 11:50:36 +0530

    blogify_fourth_commit

--- blogging/routes/blog.js
+++ blogging/routes/blog.js
@@ -1,29 +1,28 @@
-const { Router } = require("express");
-const multer = require("multer");
-const path = require("path");
+const { Router }=require("express")
+const multer=require("multer")
+const path=require("path")
 
 const Blog=require('../models/blog')
 const Comment=require('../models/comment')
 
-const router = Router();
+const router=Router();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.resolve(`./public/uploads/`));
+const storage=multer.diskStorage({
+  destination:function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads/`))
   },
   filename: function (req, file, cb) {
-    const fileName = `${Date.now()}-${file.originalname}`;
+    const fileName = `${Date.now()}-${file.originalname}`
     cb(null, fileName);
   },
-});
-
-const upload = multer({ storage: storage });
+})
+const upload = multer({ storage: storage })
 
 router.get("/add-new", (req, res) => {
   return res.render("addBlog", {
     user: req.user,
   });
-});
+})
 
 router.get("/:id",async(req,res)=>{
   const blog=await Blog.findById(req.params.id).populate("createdBy")
@@ -35,15 +34,14 @@ router.get("/:id",async(req,res)=>{
   })
 })
 
-
 router.post("/comment/:blogId", async (req, res) => {
   await Comment.create({
     content: req.body.content,
     blogId: req.params.blogId,
     createdBy: req.user._id,
-  });
-  return res.redirect(`/blog/${req.params.blogId}`);
-});
+  })
+  return res.redirect(`/blog/${req.params.blogId}`)
+})
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
   const { title, body } = req.body;
@@ -52,8 +50,8 @@ router.post("/", upload.single("coverImage"), async (req, res) => {
     title,
     createdBy: req.user._id,
     coverImageURL: `/uploads/${req.file.filename}`,
-  });
-  return res.redirect(`/blog/${blog._id}`);
-});
+  })
+  return res.redirect(`/blog/${blog._id}`)
+})
 
-module.exports = router;
+module.exports = router
